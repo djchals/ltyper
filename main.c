7c08a8b361ae8f0b2bb6c1c3cb6e10427d0a2877
djchals <djtv93@yahoo.es>
2020-08-27 12:14:47 +0200

    filter can show special characters (not finalized)

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 bool flag_dentro_menus,flag_dentro_texto;
 void bucle_menus();
 int main(){
+    
     /*  Initialize ncurses  */
     if((mainwin=initscr())==NULL){
         fprintf(stderr, "Error initializing ncurses.\n");
@@ -24,6 +25,7 @@ int main(){
     
     _init_ncurses();//necesario para mostrar los menus y las cajas, etc...
     _init_json();//necesario para leer el archivo db_texts.json
+
     bucle_menus();
     return 0;
 }
@@ -92,11 +94,11 @@ void muestra_texto(int act_id_texto){
     pos_w_actual=act_ini_w;
     pos_h_actual=act_ini_h;   
     //
-    
+
     //obtenemos el texto que hay que repetir
     char *array_puntero_texto=obten_texto(act_id_texto);//id_texto es una variable global
     long_texto=strlen(array_puntero_texto);
-    char todo_texto[long_texto];
+    unsigned char todo_texto[long_texto];
     memcpy(todo_texto,array_puntero_texto,long_texto);
     //
 
@@ -115,16 +117,17 @@ void muestra_texto(int act_id_texto){
         //Lo recorremos de nuevo para imprimir los caracteres dentro de la caja, ya sé que es un desastre lógico hacer el bucle dos veces pero no lo veo de otra manera, pq sino box() se lia
         for(i=0;i<long_texto;i++){
             if(todo_texto[i]==salto){
-                mvprintw(act_ini_h, act_ini_w, "%c",161);//imprimimos elcaracter
+                mvprintw(act_ini_h, act_ini_w, "%c",todo_texto[i]);//imprimimos elcaracter
 
                 act_ini_h++;
                 act_ini_w=ini_w;
                 num_cols_texto++;
-            }else{
+            }else{                
                 mvprintw(act_ini_h, act_ini_w, "%c",todo_texto[i]);//imprimimos elcaracter
                 act_ini_w++;
             }
         }
+        num_cols_texto++;//añadimos el salto de línea al inal para que se vea la última línea
     }
     wresize(childwin,alto_caja+num_cols_texto,ancho_caja);
     box(childwin, 0, 0);
@@ -132,9 +135,20 @@ void muestra_texto(int act_id_texto){
     move(pos_h_actual,pos_w_actual);
  
     //iniciamos el bucle de lectura y comprobación de tecla pulsada
-    i_row=0;//,i_col=0;
+    i_row=0;int ch;unsigned char tmp_special_char[2];
+    
+    //esto será necesario para el tratamiento de los carácteres especiales
+    *(tmp_special_char+0)=195;
+    tmp_special_char[2]=0x00;
+    //
+    
     while(i_row<long_texto){
-        tecla_leida=leer_tecla(todo_texto[i_row]);
+        ch=getch();
+        move(pos_h_actual,pos_w_actual);// colocamos el cursor en la posición que le toca
+
+        
+        tecla_leida=(ch==todo_texto[i_row]);//tecla_leida=0 si es incorrecto, 1 si es correcto
+
         switch(tecla_leida){
             case 0://KO tecla incorrecta
                 curs_set(0);//desactivamos el cursor pq no queremos que se vea como se mueve durante este proceso
@@ -145,7 +159,13 @@ void muestra_texto(int act_id_texto){
                 move(pos_h_actual,pos_w_actual);// colocamos el cursor en la posición que le toca
                 attron(COLOR_PAIR(C_LETRA_ERR));
                 if(todo_texto[i_row]!=ENTER){
-                    mvprintw(pos_h_actual, pos_w_actual, "%c",todo_texto[i_row]);//Repetimos el carácter correcto
+                    //AQUÍ LOS CARACTERES ESPECIALES
+                    if(todo_texto[i_row]==195){
+                        *(tmp_special_char+1)=todo_texto[i_row+1];
+                        mvprintw(pos_h_actual, pos_w_actual, "%s",tmp_special_char);         
+                    }else{
+                        mvprintw(pos_h_actual, pos_w_actual, "%c",todo_texto[i_row]);//Repetimos el carácter correcto
+                    }
                 }else{
                     mvprintw(pos_h_actual, pos_w_actual, "%c"," ");//Escribimos un espacio 
                 }
@@ -156,28 +176,41 @@ void muestra_texto(int act_id_texto){
                 if(todo_texto[i_row]!=ENTER){
                     //Marcamos el carácter en negrita
                     attron(A_BOLD);
-                    mvprintw(pos_h_actual, pos_w_actual, "%c",todo_texto[i_row]);
+                    //comprobamos si es un carácter normal o un carácter especial. Si es un carácter normal ocupa 1byte, pero si es un carácter especial ocupa 2 (á,é....)
+                    if(isalnum(todo_texto[i_row]) || ispunct(todo_texto[i_row]) || isspace(todo_texto[i_row])){
+                        mvprintw(pos_h_actual, pos_w_actual, "%c",todo_texto[i_row]);
+                    }else{
+                        //AQUÍ LOS CARACTERES ESPECIALES
+                        if(todo_texto[i_row]==195){
+                            *(tmp_special_char+1)=todo_texto[i_row+1];
+                            mvprintw(pos_h_actual, pos_w_actual, "%s",tmp_special_char);     
+                                        //si ue así es que venimos de pulsar una tecla correcta y lo anterior ue un caracter especial que ocupan el doble de bytes, por tanto sumamos i_row
+                            i_row++;
+                            ch=getch();//como hemos pulsado una combinación de teclas para que salga el puto carácter leemos otra y la desecharemos        
+                        }
+                    }
                     attroff(A_BOLD);
                     pos_w_actual++;//corremos una posición
                 }else{
-                        pos_w_actual=ini_w;
-                        pos_h_actual++;
-                }
+                    pos_w_actual=ini_w;
+                    pos_h_actual++;
+                }                            
+                
+
                 i_row++;
                 if(act_letra_err){
                     attron(COLOR_PAIR(C_LETRA_OK));
                     act_letra_err=false;
+
                 }
                 move(pos_h_actual,pos_w_actual);// colocamos el cursor en la nueva posición
                 break;
         }
+                mvprintw(27, 0, "uego al 21");//imprimimos elcaracte si comento esta linea sale el error si me equivoco de caracter
+
     }
+
     finalizar();
-}
-int leer_tecla(int comprueba_letra) {
-    int ch=getch();
-    return (ch==comprueba_letra);
-    
 }
 void muestra_errores(void){
     attron(COLOR_PAIR(C_LETRA_OK));
