548934c342a4feb5632863917c8acac0c7c15647
djchals <djtv93@yahoo.es>
2020-09-02 01:30:45 +0200

    updated cadenas_menus.h

--- cadenas_menus.h
+++ cadenas_menus.h
@@ -1,1 +1,15 @@
-//I must fill in this file later
+//main.c
+#define ET_ERRORS "Errores"
+#define ET_REPEAT_THE_TEXT "Repite el texto que ves a continuación"
+#define ET_OPTION0 "Salir del programa"
+#define ET_OPTION11 "Cancelar texto"
+#define ET_OPTION12 "Repetir texto"
+#define ET_OPTION2 "Volver al menú"
+#define ET_TIME "Tiempo"
+#define ET_TIME_OVER "SE TE HA ACABADO EL TIEMPO!!"
+#define ET_PRESS_KEY_CONTINUE "Presiona cualquier tecla para continuar..."
+#define ET_YOUR_SCORE "Has obtenido la siguiente puntuación:"
+#define ET_PPM "Pulsaciones/min"
+
+//menus.h
+#define ET_ENUNCIADO_MENU "Mecacurses - Elige una opción"
